59097dc1b38d6ae8ff72fa628b9e10e7178d31c6
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-07-02 10:43:32 -0300

    feat: Finalizando o formulario

--- src/componentes/Form/index.js
+++ src/componentes/Form/index.js
@@ -1,7 +1,19 @@
+import DropdownList from '../DropdownList';
 import TextField from '../TextField';
 import './Form.css';
 
 const Form = () => {
+
+  const team = [
+    'Programação',
+    'Front-end',
+    'Data Science',
+    'Devops',
+    'UX e Desing',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
+
   return (
     <section className="form">
       <form>
@@ -9,6 +21,7 @@ const Form = () => {
         <TextField label="Nome" placeholder="Digite o seu nome"/>
         <TextField label="Cargo" placeholder="Digite o seu cargo"/>
         <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+        <DropdownList labbel="Time" itens={team}/>
       </form>
     </section>
   )
