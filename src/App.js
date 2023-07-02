4d0a79c55397aa3a5a752df78c2005b47e0031b0
Jonathas Silva <jonathas_alves5@hotmail.com>
2023-07-02 13:55:50 -0300

    feat: Registrando interação do usuario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Form from './componentes/Form';
 
 function App() {
+
+  const [cooperators, setCooperators] = useState([]);
+
+  const onNewCooperatorInclude = (cooperator) => {
+    setCooperators([...cooperators, cooperator])
+    console.log(cooperators)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form onRegisteredCooperator={cooperator => onNewCooperatorInclude(cooperator)} />
     </div>
   );
 }
